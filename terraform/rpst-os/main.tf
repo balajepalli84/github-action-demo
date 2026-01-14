56240cf3a8a13a5845a02b18c67568dbbd98f089
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2026-01-14 07:52:50 -0600

    Add random suffix to bucket name and clean outputs
    Added a random string resource for bucket suffix and updated bucket name to include this suffix. Removed unused output for file names.

--- terraform/rpst-os/main.tf
+++ terraform/rpst-os/main.tf
@@ -5,28 +5,40 @@ provider "oci" {
   config_file_profile = "DEFAULT"
 }
 
-# -----------------------------
+# ------------------------------------------------
+# Random suffix for bucket name
+# ------------------------------------------------
+resource "random_string" "bucket_suffix" {
+  length  = 8
+  lower   = true
+  upper   = false
+  numeric = true
+  special = false
+}
+
+# ------------------------------------------------
 # Namespace
-# -----------------------------
+# ------------------------------------------------
 data "oci_objectstorage_namespace" "ns" {
   compartment_id = var.tenancy_ocid
 }
 
-# -----------------------------
-# 1) CREATE bucket
-# -----------------------------
+# ------------------------------------------------
+# 1) CREATE bucket with random name
+# ------------------------------------------------
 resource "oci_objectstorage_bucket" "created" {
   compartment_id = var.compartment_ocid
   namespace      = data.oci_objectstorage_namespace.ns.namespace
-  name           = var.bucket_name
+
+  name = "${var.bucket_name_prefix}-${random_string.bucket_suffix.result}"
 
   access_type  = "NoPublicAccess"
   storage_tier = "Standard"
 }
 
-# -----------------------------
-# 2) READ the same bucket using data source
-# -----------------------------
+# ------------------------------------------------
+# 2) READ the SAME bucket using data source
+# ------------------------------------------------
 data "oci_objectstorage_bucket" "bucket" {
   namespace = data.oci_objectstorage_namespace.ns.namespace
   name      = oci_objectstorage_bucket.created.name
@@ -34,17 +46,13 @@ data "oci_objectstorage_bucket" "bucket" {
   depends_on = [oci_objectstorage_bucket.created]
 }
 
-# -----------------------------
-# 3) LIST objects from the bucket
-# -----------------------------
+# ------------------------------------------------
+# 3) LIST objects
+# ------------------------------------------------
 data "oci_objectstorage_objects" "objs" {
   namespace = data.oci_objectstorage_namespace.ns.namespace
   bucket    = data.oci_objectstorage_bucket.bucket.name
   prefix    = var.prefix
-}
-
-output "bucket_namespace" {
-  value = data.oci_objectstorage_namespace.ns.namespace
 }
 
 output "bucket_name" {
@@ -57,11 +65,4 @@ output "object_names" {
 
 output "object_count" {
   value = length(data.oci_objectstorage_objects.objs.objects)
-}
-
-output "file_names_only" {
-  value = [
-    for o in data.oci_objectstorage_objects.objs.objects :
-    element(reverse(split("/", o.name)), 0)
-  ]
 }
