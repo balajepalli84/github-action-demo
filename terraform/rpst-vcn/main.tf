0df872a0e0787e556c12d56f35d56e4c1117709c
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2026-01-14 11:30:23 -0600

    Change OCI provider profile from 'rpst' to 'DEFAULT'

--- terraform/rpst-vcn/main.tf
+++ terraform/rpst-vcn/main.tf
@@ -2,7 +2,7 @@ provider "oci" {
   region              = var.region
   tenancy_ocid        = var.tenancy_ocid
   auth                = "securitytoken"
-  config_file_profile = "rpst"
+  config_file_profile = "DEFAULT"
 }
 
 resource "oci_core_virtual_network" "my_tf_vcn" {
