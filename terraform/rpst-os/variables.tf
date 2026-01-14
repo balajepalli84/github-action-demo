2b5b4e387430a74ff0c7b84e9c7bb20f020a2219
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2026-01-14 07:49:14 -0600

    Enhance variables for Object Storage in Terraform
    Updated compartment_ocid description to include Object Storage context and added new_bucket_name variable for creating a new Object Storage bucket.

--- terraform/rpst-os/variables.tf
+++ terraform/rpst-os/variables.tf
@@ -10,9 +10,16 @@ variable "tenancy_ocid" {
 
 variable "compartment_ocid" {
   type        = string
-  description = "OCID of the compartment to deploy resources into"
+  description = "OCID of the compartment to deploy resources into (also used for Object Storage bucket creation)"
 }
 
+# New bucket name to CREATE
+variable "new_bucket_name" {
+  type        = string
+  description = "Name of the new Object Storage bucket to create"
+}
+
+# Existing bucket name to READ (your current variable)
 variable "bucket_name" {
   type        = string
   description = "Name of the existing Object Storage bucket to read"
